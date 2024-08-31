ea5396730932d059bcd1df87b80394837de9856c
Ankit Kumar <66104733+ankitrajsh@users.noreply.github.com>
2024-08-31 14:26:38 +0530

    Add files via upload

---
+++ punkt_downloader.py
@@ -0,0 +1,6 @@
+import nltk
+# nltk.download('punkt')
+nltk.download('wordnet')
+# nltk.download('wordnet', download_dir='/content/nltk_data/')
+# nltk.download('omw-1.4')  # Optional, but sometimes needed for wordnet
+# print(nltk.data.path)
